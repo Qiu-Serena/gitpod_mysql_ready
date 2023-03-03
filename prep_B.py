7628998d9188d0f4c55781b8ca70543e98f83a66
Qiu-Serena <qiu.serena@iisgalvanimi.edu.it>
2023-03-03 09:59:01 +0000

    prep

--- prep_B.py
+++ prep_B.py
@@ -21,3 +21,17 @@ mycursor = mydb.cursor()
 
 mycursor.execute ("CREATE TABLE Mammiferi (Id INT AUTO_INCREMENT PRIMARY KEY, Nome_Proprio VARCHAR(255), Razza VARCHAR(255), Peso (int), Eta (int)))
 
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO Mammiferi (Id, Nome_Proprio (varchar), Razza (varchar), Peso (int), Eta (int)) VALUES (%s, %s,%s,%s)"
+val = ("", "Highway 21")
+("John", "Highway 21")
+("John", "Highway 21")
+("John", "Highway 21")
+("John", "Highway 21")
+("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
